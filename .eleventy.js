f2d94256efd2889a52bd748e79813c50f066d071
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2018-12-12 12:07:38 +0100

    add bootstrap template

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+module.exports = function(eleventyConfig) {
+  const result = {
+    templateFormats: [
+      "html",
+      "jpg",
+      "js",
+      "liquid",
+      "json",
+      "ico", // for favicon
+      "css" // css is not yet a valid template extension
+    ],
+    passthroughFileCopy: true
+  };
+
+  return result;
+};
