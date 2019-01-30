31e3fe925342cb29d3f2a0a5e2d871b2f0b8b953
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2019-01-30 08:26:01 +0100

    use graphql-ld in other data files too

--- _data/importantdates.js
+++ _data/importantdates.js
@@ -1,94 +1,33 @@
-const newEngine = require('@comunica/actor-init-sparql-rdfjs').newEngine;
-const N3 = require('n3');
-const Q = require('q');
-const fs = require('fs');
+const GraphQLExecutor = require('../graphqlexecutor');
 
 const monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
 ];
 
 module.exports = async () => {
- const result = await getImportantDates('./_data/workshop-data.ttl');
-
- return result;
-};
-
-async function getImportantDates(path) {
-  const deferred = Q.defer();
-  const rdfjsSource = await getRDFjsSourceFromFile(path);
-  const engine = newEngine();
-  const importantDates = [];
-
-  engine.query(`SELECT * {
-     <https://kgb-workshop.org/resources/Event/kgb2019> <http://schema.org/subEvent> ?s.
-     
-     ?s a <http://schema.org/Event>;
-        <http://schema.org/name> ?title;
-        <http://schema.org/description> ?desc;
-        <http://schema.org/startDate> ?date.
-  }`,
-    {sources: [{type: 'rdfjsSource', value: rdfjsSource}]})
-    .then(function (result) {
-      result.bindingsStream.on('data', async function (data) {
-        data = data.toObject();
-
-        importantDates.push({
-          title: data['?title'].value,
-          date: data['?date'].value,
-          description: data['?desc'].value,
-        });
-      });
-
-      result.bindingsStream.on('end', function () {
-        importantDates.sort( (a, b) => {
-          return new Date(a.date) - new Date(b.date);
-        });
-
-        importantDates.forEach(importantDate => {
-          const date = new Date(importantDate.date);
-          importantDate.date = {
-            day: date.getDate(),
-            monthYear: monthNames[date.getMonth()] + ', ' + date.getFullYear()
-          };
-        });
-
-        deferred.resolve(importantDates);
-      });
+  const executor = new GraphQLExecutor();
+  const result = await executor.query('./_data/workshop-data.ttl', {
+    "title": { "@id": "http://schema.org/name", "@singular": true },
+    "description": { "@id": "http://schema.org/description", "@singular": true },
+    "date": { "@id": "http://schema.org/startDate", "@singular": true },
+    "subEvent": { "@reverse": "http://schema.org/subEvent", "@singular": true },
+    "Event": "http://schema.org/Event",
+    "workshop": "https://kgb-workshop.org/resources/Event/kgb2019"
+  },`{... on Event {title description date subEvent(_:workshop)}}`, importantDates => {
+    importantDates.sort( (a, b) => {
+      return new Date(a.date) - new Date(b.date);
     });
 
-  return deferred.promise;
-}
-
-/**
- * This method returns an RDFJSSource of an file
- * @param {string} path: path of the file
- * @returns {Promise}: a promise that resolve with the corresponding RDFJSSource
- */
-function getRDFjsSourceFromFile(path) {
-  const deferred = Q.defer();
-
-  fs.readFile(path, 'utf-8', (err, data) => {
-    if (err) throw err;
-
-    const parser = N3.Parser();
-    const store = N3.Store();
-
-    parser.parse(data, (err, quad, prefixes) => {
-      if (err) {
-        deferred.reject();
-      } else if (quad) {
-        store.addQuad(quad);
-      } else {
-        const source = {
-          match: function (s, p, o, g) {
-            return require('streamify-array')(store.getQuads(s, p, o, g));
-          }
-        };
-
-        deferred.resolve(source);
-      }
+    importantDates.forEach(importantDate => {
+      const date = new Date(importantDate.date);
+      importantDate.date = {
+        day: date.getDate(),
+        monthYear: monthNames[date.getMonth()] + ', ' + date.getFullYear()
+      };
     });
+
+    return importantDates;
   });
 
-  return deferred.promise;
-}
+  return result;
+};
--- _data/workshop.js
+++ _data/workshop.js
@@ -1,83 +1,24 @@
-const newEngine = require('@comunica/actor-init-sparql-rdfjs').newEngine;
-const N3 = require('n3');
-const Q = require('q');
-const fs = require('fs');
+const GraphQLExecutor = require('../graphqlexecutor');
 
 const monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
 ];
 
 module.exports = async () => {
- const result = await getWorkhopDetails('./_data/workshop-data.ttl');
+  const executor = new GraphQLExecutor();
+  const result = await executor.query('./_data/workshop-data.ttl', {
+    "title": { "@id": "http://schema.org/name", "@singular": true },
+    "location": { "@id": "http://schema.org/location", "@singular": true},
+    "mainEvent": { "@id": "http://schema.org/superEvent", "@singular": true},
+    "date": { "@id": "http://schema.org/startDate", "@singular": true},
+    "Event": "http://schema.org/Event"
+  },`{... on Event {title, location, mainEvent, date}}`, result => {
+    result = result[0];
+    const date = new Date(result.date);
+    result.date = `${date.getDate()} ${monthNames[date.getMonth()]} ${date.getFullYear()}`;
+
+    return result;
+  });
 
  return result;
 };
-
-async function getWorkhopDetails(path) {
-  const deferred = Q.defer();
-  const rdfjsSource = await getRDFjsSourceFromFile(path);
-  const engine = newEngine();
-
-  engine.query(`SELECT * {
-     ?s a <http://schema.org/Event>;
-        <http://schema.org/name> ?title;
-        <http://schema.org/location> ?loc;
-        <http://schema.org/superEvent> ?mainEvent;
-        <http://schema.org/startDate> ?date.
-  }`,
-    {sources: [{type: 'rdfjsSource', value: rdfjsSource}]})
-    .then(function (result) {
-      result.bindingsStream.on('data', async function (data) {
-        data = data.toObject();
-
-        const date = new Date(data['?date'].value);
-
-        deferred.resolve({
-          title: data['?title'].value,
-          date: `${date.getDate()} ${monthNames[date.getMonth()]} ${date.getFullYear()}`,
-          location: data['?loc'].value,
-          mainEvent: data['?mainEvent'].value,
-        });
-      });
-
-      result.bindingsStream.on('end', function () {
-
-      });
-    });
-
-  return deferred.promise;
-}
-
-/**
- * This method returns an RDFJSSource of an file
- * @param {string} path: path of the file
- * @returns {Promise}: a promise that resolve with the corresponding RDFJSSource
- */
-function getRDFjsSourceFromFile(path) {
-  const deferred = Q.defer();
-
-  fs.readFile(path, 'utf-8', (err, data) => {
-    if (err) throw err;
-
-    const parser = N3.Parser();
-    const store = N3.Store();
-
-    parser.parse(data, (err, quad, prefixes) => {
-      if (err) {
-        deferred.reject();
-      } else if (quad) {
-        store.addQuad(quad);
-      } else {
-        const source = {
-          match: function (s, p, o, g) {
-            return require('streamify-array')(store.getQuads(s, p, o, g));
-          }
-        };
-
-        deferred.resolve(source);
-      }
-    });
-  });
-
-  return deferred.promise;
-}
